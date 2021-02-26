c62f2ad1030f7b33ea79b2241061a6396c9bff25
Flavio Wuensche <f.wuensche@gmail.com>
2021-02-26 21:37:02 +0100

    posts#index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+
+const getPosts = () =>
+  fetch("https://dev.to/api/articles?username=fwuensche").then((res) =>
+    res.json()
+  )
 
 function App() {
+  const [posts, setPosts] = React.useState([])
+
+  React.useEffect(() => {
+    getPosts().then((data) => setPosts(data))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Antelo Live</p>
+        {posts.map((post) => (
+          <a
+            className="App-link"
+            href={post.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {post.title}
+          </a>
+        ))}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
