159cef025c5ff4d7bbe8d7f138782dd5ad59d0fc
Flavio Wuensche <f.wuensche@gmail.com>
2021-03-01 23:51:55 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
 import Posts from "./components/Posts"
 import Post from "./components/Post"
 import "./App.css"
@@ -9,7 +9,9 @@ const App = () => (
     <div className="App">
       <header className="App-header">
         <Switch>
-          <Route exact path="/" render={() => <Posts />} />
+          <Route exact path="/">
+            <Posts />
+          </Route>
           <Route path="/posts/:id">
             <Post />
           </Route>
