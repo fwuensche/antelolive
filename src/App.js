50c59a0c61c286ede3e60db81728f4f94f977362
Flavio Wuensche <f.wuensche@gmail.com>
2021-03-02 00:19:16 +0100

    live 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
 import Posts from "./components/Posts"
 import Post from "./components/Post"
 import "./App.css"
