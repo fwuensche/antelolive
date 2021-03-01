d9d82fc571a09d2870cc3e7049a41a7a2983db0b
Flavio Wuensche <f.wuensche@gmail.com>
2021-03-02 00:22:44 +0100

    Revert "live 3"
    This reverts commit 50c59a0c61c286ede3e60db81728f4f94f977362.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { HashRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Posts from "./components/Posts"
 import Post from "./components/Post"
 import "./App.css"
